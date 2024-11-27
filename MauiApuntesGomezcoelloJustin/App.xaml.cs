bf776b5013365e05fdd7d29f646c8338cf14037c
cristian.gomezcoello <cristian.gomezcoello@DESKTOP-C1RQP2B>
2024-11-26 20:07:40 -0500

    Add project files.

---
+++ MauiApuntesGomezcoelloJustin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApuntesGomezcoelloJustin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiApuntesGomezcoelloJustin/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApuntesGomezcoelloJustin
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
