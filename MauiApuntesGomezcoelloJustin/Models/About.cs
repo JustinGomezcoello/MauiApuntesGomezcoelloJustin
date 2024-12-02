521155b8d478e8560decafc5e6f6f6e5d2e22522
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2024-12-01 20:40:55 -0500

    cambio de informacion sobre mi

--- MauiApuntesGomezcoelloJustin/Models/About.cs
+++ MauiApuntesGomezcoelloJustin/Models/About.cs
@@ -11,7 +11,7 @@ namespace MauiApuntesGomezcoelloJustin.Models
         public string Title => "Justin Gomezcoello";
         public string Version => AppInfo.VersionString;
         public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "Soy un chico de 20 años que le encanta pasar horas y horas en la pc investigando nuevos temas de su interés, al igual que le gusta mucho jugar fútbol y ir al gimnasio.";
+        public string Message => "Soy un chico de 20 años que le encanta pasar horas y horas en la pc investigando nuevos temas de mi interés, al igual que me gusta mucho jugar fútbol y ir al gimnasio ya que esto me desestresa.";
         public string Icon { get; set; }
     
     }
